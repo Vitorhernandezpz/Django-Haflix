895ec6788c2cfba15bb512c3e83e28fdba928955
vitor.perez <vitorhernandezp7@gmail.com>
2024-11-13 22:12:04 -0300

    Ajustando link dos episodeos para apareceram em tela cheia ao inves de abrir direto no yt Criação de variaveis personalizada para as views.

--- filme/views.py
+++ filme/views.py
@@ -1,3 +1,5 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
@@ -13,6 +15,14 @@ class Homefilmes(ListView):
 class Detalhesfilmes(DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
+
+    # super executa a função da super classe, no caso a detailview
+    def get_context_data(self, **kwargs):
+        context = super(Detalhesfilmes, self).get_context_data()
+        # filtrar a minha tabela de filmes pegando os filmes cuja categoria é igual a categoria do filme da página (object)
+        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[0:5]
+        context["filmes_relacionados"] = filmes_relacionados
+        return context
 
 #def homepage(request):
 #    return render(request,'homepage.html')
