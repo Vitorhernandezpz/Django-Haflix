edc65eef14d07a13db22534741e768244fdaf692
vitor.perez <vitorhernandezp7@gmail.com>
2024-12-20 17:58:05 -0300

    Criação das páginas de editar perfil e mudar senha usuario

--- filme/views.py
+++ filme/views.py
@@ -1,14 +1,15 @@
 from lib2to3.fixes.fix_input import context
 
 from django.shortcuts import render, redirect, reverse
-from .models import Filme
-from django.views.generic import TemplateView, ListView, DetailView, FormView
+from .models import Filme, Usuario
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CriarContaForm
+from .forms import CriarContaForm, FormHomepage
 
 # Create your views here.
-class Homepage(TemplateView):
+class Homepage(FormView):
     template_name = "homepage.html"
+    form_class = FormHomepage
 
     def get(self, request, *args, **kwargs):
        if request.user.is_authenticated:
@@ -16,6 +17,13 @@ class Homepage(TemplateView):
        else:
         return super().get(request, *args, **kwargs) # Redireciona para homepage, caso user não está autenticado
 
+    def get_success_url(self):
+        email = self.request.POST.get("email")
+        usuarios = Usuario.objects.filter(email=email)
+        if usuarios:
+            return reverse("filme:login")
+        else:
+            return reverse("filme:criarconta")
 
 class Homefilmes(LoginRequiredMixin, ListView):
     template_name = "homefilmes.html"
@@ -56,8 +64,14 @@ class Pesquisafilme(LoginRequiredMixin, ListView):
         else:
             return None
 
-class Paginaperfil(LoginRequiredMixin, TemplateView):
+class Paginaperfil(LoginRequiredMixin, UpdateView):
     template_name = "editarperfil.html"
+    model = Usuario
+    # Campos que serão atualizados de acordo com o modelo do AbstractUser
+    fields = ['first_name','last_name','email']
+
+    def get_success_url(self):
+        return reverse('filme:homefilmes')
 
 class Criarconta(FormView):
     template_name = "criarconta.html"
